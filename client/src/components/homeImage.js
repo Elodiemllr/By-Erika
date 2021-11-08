5e85c4e554549f04d6442210930bdce12f04a888
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-11-08 15:48:20 +0100

    home image css

--- client/src/components/homeImage.js
+++ client/src/components/homeImage.js
@@ -1,23 +1,21 @@
 const HomeImage = () => {
     return (
         <div className="homeImage">
-            <div className="homeImage__boxs">
-                <img
-                    src="./img/box1.jpg"
-                    alt="box "
-                    className="homeImage__box"
-                ></img>
-                <img
-                    src="./img/box2.jpg"
-                    alt="box"
-                    className="homeImage__box"
-                ></img>
-                <img
-                    src="./img/box3.jpg"
-                    alt="box"
-                    className="homeImage__box"
-                ></img>
-            </div>
+            <img
+                src="./img/box1.jpg"
+                alt="box "
+                className="homeImage__box"
+            ></img>
+            <img
+                src="./img/box2.jpg"
+                alt="box"
+                className="homeImage__box"
+            ></img>
+            <img
+                src="./img/box3.jpg"
+                alt="box"
+                className="homeImage__box"
+            ></img>
         </div>
     );
 };
